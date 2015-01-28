467be470f2ca429649449c9c9b01c3d0a1ec433a
Roland Lopes <roland@rewardops.com>
2015-01-28 18:05:37 -0500

    rlopes: Added support for boolean type validation

--- lib/params_validator/validator/type.rb
+++ lib/params_validator/validator/type.rb
@@ -45,6 +45,18 @@ module ParamsValidator
         super || value.kind_of?(Hash)
       end
     end
+
+    class TypeBoolean < Type
+      def valid?(value)
+        super || to_bool(value) rescue false
+      end
+
+      def is_bool(value)
+        return true  if value == true   || value =~ (/(true|t|yes|y|1)$/i)
+        return true  if value == false  || value.blank? || value =~ (/(false|f|no|n|0)$/i)
+        raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+      end
+    end
   end
 end
 
